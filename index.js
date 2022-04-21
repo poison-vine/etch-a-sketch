19928bf3beaedd857b8721ffd3cd328915dc3c4e
poison-vine <vine_trigger@live.com>
2022-04-21 12:27:33 -0500

    fixes styles in index.css fixes code in index.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const container = document.querySelector("#container");
+const resetButton = document.querySelector('#resetButton');
 
-
-for(let i=0; i< 16*16; i++)
+if(resetButton)
 {
-    const div = document.createElement('div');
-    div.className = "grid-box";
-    container.append(div);
+    resetButton.addEventListener("click" , (e)=>{
+        let d = prompt("Enter n for an n x n grid");
+        
+        if(d > 100)alert('n must be <= 100');
+        else resetGrid(parseInt(d));
+
+    });
 }
+
+function resetGrid(dimensions){
+    container.innerHTML ='';
+    container.style.gridTemplateRows = "auto ".repeat(dimensions);
+
+    for(let i=0; i< dimensions*dimensions; i++)
+    {
+        const div = document.createElement('div');
+        div.className = "grid-box";
+        container.append(div);
+    }
+
+
+}
+
