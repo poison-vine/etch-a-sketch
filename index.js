b2b30fc04c6fd776b012ea760476aa21ccb10cee
poison-vine <vine_trigger@live.com>
2022-04-21 11:43:32 -0500

    adds code to index.js to create 16x16 grid and adds styles to index.css

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const container = document.querySelector("#container");
 
+
+for(let i=0; i< 16*16; i++)
+{
+    const div = document.createElement('div');
+    div.className = "grid-box";
+    container.append(div);
+}
